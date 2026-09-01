d7498cd6edee8f91767aabc0ee3a93c118db2613
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-09-01 21:08:32 +0300

    feat: add TypeScript types

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+export interface MvolaClientConfig {
+  consumerKey: string;
+  consumerSecret: string;
+  sandbox?: boolean;
+}
+
+export type TokenResponse = {
+  access_token: string;
+  scope: string;
+  token_type: string;
+  expires_in: number;
+};
+
+export type TransactionParty = {
+  key: "msisdn";
+  value: string;
+};
+
+export type TransactionMetadata = {
+  key: string;
+  value: string;
+};
+
+export interface InitiateTransactionParams {
+  amount: number;
+  currency?: string;
+  description: string;
+  debitParty: string;
+  creditParty: string;
+  partnerName?: string;
+  callbackUrl?: string;
+  language?: "FR" | "MG";
+}
+
+export type TransactionResponse = {
+  status: string;
+  serverCorrelationId: string;
+  notificationMethod: string;
+};
+
+export type TransactionStatus = {
+  status: string;
+  serverCorrelationId: string;
+  notificationMethod: string;
+  objectReference: string;
+};
+
+export type TransactionDetails = {
+  amount: string;
+  currency: string;
+  transactionReference: string;
+  transactionStatus: string;
+  creationDate: string;
+  requestDate: string;
+  debitParty: TransactionParty[];
+  creditParty: TransactionParty[];
+  metadata: TransactionMetadata[];
+  fees: { feeAmount: string }[];
+};
