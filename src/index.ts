c8d25fed6ff15e97289e9c6b3b131d1381d7f7a0
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-09-01 21:10:05 +0300

    feat: export public API

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { MVolaClient } from "./client";
+export type {
+  MvolaClientConfig,
+  TokenResponse,
+  TransactionParty,
+  TransactionMetadata,
+  InitiateTransactionParams,
+  TransactionResponse,
+  TransactionStatus,
+  TransactionDetails,
+} from "./types";
